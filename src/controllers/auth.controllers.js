aa765c6334b4aea7805ed344b17ffac25a557fdb
Caio Pires <caiogodoi_92@hotmail.com>
2023-08-04 23:02:25 -0300

    create signUp

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,7 +2,7 @@ import { db } from '../database/database.connection.js'
 import bcrypt from 'bcrypt'
 import { v4 as uuid } from 'uuid'
 
-export async function createUser(req, res) {
+export async function signUp(req, res) {
   const { name, email, password } = req.body
 
   try {
@@ -26,9 +26,32 @@ export async function createUser(req, res) {
 }
 
 export async function login(req, res) {
+  const { email, password } = req.body
+
   try {
-    //
-    res.send(newCustomers)
+    const response = await db.query('SELECT * FROM users WHERE email=$1', [
+      email
+    ])
+
+    if (response.rowCount <= 0) {
+      return res.status(401).send({ message: 'Usuario não cadastrado!' })
+    }
+
+    const user = response.rows[0]
+
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password)
+
+    if (!isPasswordCorrect)
+      return res.status(401).send({ message: 'Senha incorreta' })
+
+    const token = uuid()
+
+    await db.query('INSERT INTO sessions (token, user_id) VALUES ($1, $2)', [
+      token,
+      user.id
+    ])
+
+    res.send({ token })
   } catch (err) {
     res.status(500).send(err.message)
   }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import { createUser, login } from '../controllers/auth.controllers.js'
+import { signUp, login } from '../controllers/auth.controllers.js'
 import { userSchema } from '../schemas/user.schema.js'
 import { loginSchema } from '../schemas/login.schema.js'
 import validateSchema from '../middlewares/validateSchema.middleware.js'
 
 const authRouter = Router()
 
-authRouter.post('/signup', validateSchema(userSchema), createUser)
-authRouter.post('/signin', validateSchema(loginSchema), login)
+authRouter.post('/signup', validateSchema(userSchema), signUp)
+authRouter.post('/login', validateSchema(loginSchema), login)
 
 export default authRouter
